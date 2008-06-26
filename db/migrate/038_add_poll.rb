623e58fe1872bc386a42cd5ca4862ef115a36f0d
Bob Sturim <bob@sturim.org>
2008-06-26 01:18:10 +0000

    Added polls table

---
+++ db/migrate/038_add_poll.rb
@@ -0,0 +1,14 @@
+class AddPoll < ActiveRecord::Migration
+  def self.up
+    create_table :polls do |t|
+      t.column :title,  :string, :limit => 120, :null => false
+      t.column :expiration_date, :date, :null => false
+      t.column :lock_version, :integer, :default => 0
+      t.column :created_at, :datetime, :null => false
+    end
+  end
+
+  def self.down
+    drop_table :polls
+  end
+end
