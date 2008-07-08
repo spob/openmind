c1290950a94a3fbdcd170236a187958eac1252ab
Bob Sturim <bob@sturim.org>
2008-07-08 18:26:24 +0000

    Got unit test working

--- test/unit/forum_test.rb
+++ test/unit/forum_test.rb
@@ -9,4 +9,16 @@ class ForumTest < Test::Unit::TestCase
     assert !forum.valid?
     assert forum.errors.invalid?(:name)
   end
+  
+  def test_invalid_too_long
+    forum = Forum.new(:name => "0123456789012345678901234567890123456789012345678901")
+    assert !forum.valid?
+    assert_equal "is too long (maximum is 50 characters)", 
+      forum.errors.on(:name)
+  end
+  
+  def test_valid_with_attributes
+    forum = Forum.new(:name => "bugs")
+    assert forum.valid?
+  end
 end
