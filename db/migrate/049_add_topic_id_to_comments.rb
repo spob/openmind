b98167704ab9601b89f266709f0c262a6664074b
Bob Sturim <bob@sturim.org>
2008-07-11 12:21:09 +0000

    Got migration script to work

--- db/migrate/049_add_topic_id_to_comments.rb
+++ db/migrate/049_add_topic_id_to_comments.rb
@@ -1,7 +1,7 @@
 class AddTopicIdToComments < ActiveRecord::Migration
   def self.up
     add_column(:comments, :topic_id, :integer, :null => true)
-    change_column(:comments, :idea_id, :integer, :null => true)
+    change_column(:comments, :idea_id, :integer, :null => true, :default => nil)
     
     add_index :comments, :topic_id, :unique => false
   end
