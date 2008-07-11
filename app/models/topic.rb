5d594e8b70391ed6bd4f5e91c73228177f9ddf74
Bob Sturim <bob@sturim.org>
2008-07-11 12:43:05 +0000

    Added unread_comment method (untested)

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -25,6 +25,13 @@ class Topic < ActiveRecord::Base
     comment.id == last_comment.id
   end
   
+  def unread_comment user
+    read = UserTopicRead.find_by_user_id_and_topic_id(user.id, id)
+    return false if last_comment.nil?
+    return true if read == nil?
+    read.update_date < last_comment.created_at
+  end
+  
   def add_user_read user
     read = UserTopicRead.find_by_user_id_and_topic_id(user.id, id)
     if read.nil?
