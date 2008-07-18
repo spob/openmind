ca1967962e79c990411cbf0f2a6566f066ebaeb1
Bob Sturim <bob@sturim.org>
2008-07-18 03:07:18 +0000

    Added date_util

---
+++ lib/date_utils.rb
@@ -0,0 +1,15 @@
+# 
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+ 
+
+class DateUtils
+  def self.time_to_datetime t
+    # convert to seconds + microseconds into a fractional number of seconds
+    seconds = t.sec + Rational(t.usec, 10**6)
+    
+    # Convert to UTC offset
+    offset = Rational(t.utc_offset, 60*60*24)
+    DateTime.new(t.year, t.month, t.day, t.hour, t.min, seconds, offset)
+  end
+end
