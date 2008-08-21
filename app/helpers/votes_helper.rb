f5f01cb009ba3caf1129a6e75a0e0b92becc4422
Bob Sturim <bob@sturim.org>
2008-08-21 01:30:41 +0000

    fixed bug when bogus allocation_type

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,7 +1,7 @@
 module VotesHelper
 
   def display_allocated_to(vote)
-    if vote.allocation.user_id.nil? 
+    if vote.allocation.class.to_s == "EnterpriseAllocation"
       vote.allocation.enterprise.name
     else
       user_display_name(vote.allocation.user)
