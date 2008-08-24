3f83b3a9c68b4c503ffabb8901b8022d16660d76
Bob Sturim <bob@sturim.org>
2008-08-24 12:09:36 +0000

    Fixed bug in order of statements

--- db/migrate/030_convert_string_to_text.rb
+++ db/migrate/030_convert_string_to_text.rb
@@ -13,8 +13,8 @@ class ConvertStringToText < ActiveRecord::Migration
     execute "UPDATE allocations set comments = substr(comments, 1, 1000) where length(comments) > 1000"
     execute "ALTER TABLE allocations MODIFY COLUMN comments varchar(1000)"
     
-    execute "ALTER TABLE announcements MODIFY COLUMN description varchar(1000)"
     execute "UPDATE announcements set description = substr(description, 1, 1000) where length(description) > 1000"
+    execute "ALTER TABLE announcements MODIFY COLUMN description varchar(1000)"
     
     execute "UPDATE comments set body = substr(body, 1, 1000) where length(body) > 1000"
     execute "ALTER TABLE comments MODIFY COLUMN body varchar(1000)"
