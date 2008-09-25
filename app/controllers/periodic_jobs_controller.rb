4b677883b483883a21520e550c34fa3584ebd975
Bob Sturim <bob@sturim.org>
2008-09-25 02:56:04 +0000

    Comment out unnecessary code

--- app/controllers/periodic_jobs_controller.rb
+++ app/controllers/periodic_jobs_controller.rb
@@ -3,12 +3,12 @@ class PeriodicJobsController < ApplicationController
   access_control [:edit, :update, :destroy, :show, :index] => 'sysadmin'
   
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [:create, :approve, :reject ],
-    :redirect_to => { :action => :index }
-  verify :method => :put, :only => [ :update ],
-    :redirect_to => { :action => :index }
-  verify :method => :delete, :only => [ :destroy ],
-    :redirect_to => { :action => :index }
+  #  verify :method => :post, :only => [:create ],
+  #    :redirect_to => { :action => :index }
+  #  verify :method => :put, :only => [ :update ],
+  #    :redirect_to => { :action => :index }
+  #  verify :method => :delete, :only => [ :destroy ],
+  #    :redirect_to => { :action => :index }
   
   def index
     @jobs = PeriodicJob.list params[:page], current_user.row_limit
