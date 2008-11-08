2e037d0c0a38aea34b03d5de0d15d3234c58616c
Bob Sturim <bob@sturim.org>
2008-11-08 20:30:45 +0000

    Ignore coverage

--- test/functional/link_sets_controller_test.rb
+++ test/functional/link_sets_controller_test.rb
@@ -1,4 +1,5 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
+require 'link_sets_controller'
 
 class LinkSetsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
