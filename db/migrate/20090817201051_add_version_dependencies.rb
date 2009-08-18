654c023fe1125adf2594f2f792341a990a62b6f1
unknown <bsturim@.SCRIBEHQ.local>
2009-08-18 17:09:37 -0400

    Add ability to create new dependency group codes

---
+++ db/migrate/20090817201051_add_version_dependencies.rb
@@ -0,0 +1,27 @@
+require "migration_helpers"
+
+class AddVersionDependencies < ActiveRecord::Migration
+  extend MigrationHelpers
+  
+  def self.up
+    create_table :release_dependencies do |t|
+      t.references :release, :null => false
+      t.integer :depends_on_id, :null => false
+      t.column :lock_version, :integer, :default => 0
+      t.integer :dependency_group_id, :null => false
+      t.timestamps
+    end
+
+    add_foreign_key(:release_dependencies, :release_id, :releases)
+    add_foreign_key(:release_dependencies, :depends_on_id, :releases)
+
+    add_index :release_dependencies, [:release_id, :depends_on_id], :unique => true
+  end
+
+  def self.down
+    remove_foreign_key(:release_dependencies, :release_id)
+    remove_foreign_key(:release_dependencies, :depends_on_id)
+
+    drop_table :release_dependencies
+  end
+end
