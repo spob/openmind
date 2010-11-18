121a3f7a7f1750de2584bb45a3982408bf4ec16b
Bob Sturim <bob@sturim.org>
2010-11-18 12:45:22 -0500

    Optimized if otp when already logged on

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,19 +2,19 @@ class AccountController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
   include OpenIdAuthentication
-  
+
   # If you want "remember me" functionality, add this before_filter to Application Controller
   before_filter :login_from_cookie, :except => [:login]
-  before_filter :login_required, :only => [ :logout ]
-  cache_sweeper :allocations_sweeper, :only => [ :login, :continue_openid ]
-  
+  before_filter :login_required, :only => [:logout]
+  cache_sweeper :allocations_sweeper, :only => [:login, :continue_openid]
+
   # say something nice, you goof!  something sweet.
   def index
     redirect_to(:action => 'signup') unless logged_in? || User.count > 0
   end
-  
+
   def login
-    return unless request.post?    
+    return unless request.post?
     @login = params[:login] # needed to remember login info in login fails
     self.current_user = User.authenticate(params[:login], params[:password])
     if logged_in?
@@ -23,14 +23,16 @@ class AccountController < ApplicationController
       flash[:error] = "Login failed...please try again"
     end
   end
-  
-  def login_otp 
+
+  def login_otp
     @login = params[:login] # needed to remember login info in login fails
-    self.current_user = User.authenticate_otp(params[:login], params[:password])
-    if self.current_user
-      logger.info "Current user is #{self.current_user.id}"
-    else
-      logger.info "current user is empty"
+    unless logged_in?
+      self.current_user = User.authenticate_otp(params[:login], params[:password])
+      if self.current_user
+        logger.info "Current user is #{self.current_user.id}"
+      else
+        logger.info "current user is empty"
+      end
     end
     if logged_in?
       # if a uri is specified, automatically navigate to it
@@ -46,47 +48,55 @@ class AccountController < ApplicationController
       end
     end
   end
-  
+
   def login_openid
-    return unless request.post?   
-    
-    @openid_url = add_trailing_slash params[:openid_url] 
-    
+    return unless request.post?
+
+    @openid_url = add_trailing_slash params[:openid_url]
+
     user = User.find_by_identity_url(@openid_url)
     if user.nil?
       flash[:error] = "No OpenMind user associated to this Open Id. Edit your user profile to update your Open Id."
       return
     end
-    
+
     status = begin_openid_authentication(@openid_url, '/account/continue_openid')
-    
-    flash[:error] = case status 
-      when :missing then 'Sorry, the OpenID is missing.'
-      when :failed  then 'Sorry, the OpenID verification failed.'
-      when :timeout then 'Timed out.'
-      when :unknown then 'Not sure what happened.'
+
+    flash[:error] = case status
+      when :missing then
+        'Sorry, the OpenID is missing.'
+      when :failed then
+        'Sorry, the OpenID verification failed.'
+      when :timeout then
+        'Timed out.'
+      when :unknown then
+        'Not sure what happened.'
     end
   end
-  
+
   def continue_openid
     status = complete_openid_authentication
-    
-    case status 
-      when :missing   then failed_login('Sorry, the OpenID server couldn\'t be found.')
-      when :canceled  then failed_login('OpenID verification was canceled.')
-      when :failed    then failed_login('Sorry, the OpenID verification failed.')
-      when :unknown   then failed_login('Not sure what happened.')
+
+    case status
+      when :missing then
+        failed_login('Sorry, the OpenID server couldn\'t be found.')
+      when :canceled then
+        failed_login('OpenID verification was canceled.')
+      when :failed then
+        failed_login('Sorry, the OpenID verification failed.')
+      when :unknown then
+        failed_login('Not sure what happened.')
       when :success
-      self.current_user = User.find_by_identity_url(openid_result[:identity_url])
-      logged_in
+        self.current_user = User.find_by_identity_url(openid_result[:identity_url])
+        logged_in
     end
   end
-  
+
   def activate
-    flash.clear  
+    flash.clear
     return if params[:id].nil? and params[:activation_code].nil?
     activator = params[:activation_code] || params[:id]
-    @user = User.find_by_activation_code(activator) 
+    @user = User.find_by_activation_code(activator)
     if @user
       if @user.activate
         redirect_to(:controller => '/account', :action => 'login')
@@ -98,7 +108,7 @@ class AccountController < ApplicationController
       flash[:error] = "Unable to activate the account: no such activation code '#{activator}'."
     end
   end
-  
+
   def signup
     @user = User.new(params[:user])
     return unless request.post?
@@ -109,7 +119,7 @@ class AccountController < ApplicationController
   rescue ActiveRecord::RecordInvalid
     render :action => 'signup'
   end
-  
+
   def logout
     #    self.current_user.forget_me if logged_in?
     #    cookies.delete :auth_token
@@ -117,30 +127,30 @@ class AccountController < ApplicationController
     flash[:notice] = "You have been logged out."
     redirect_back_or_default(:controller => '/account', :action => 'login')
   end
-  
+
   def change_password
     return unless request.post?
     @user = self.current_user
     if params[:user][:password] != params[:user][:password_confirmation]
-      flash[:error] = "Password confirmation must match" 
+      flash[:error] = "Password confirmation must match"
       render :action => 'change_password'
     elsif @user.authenticated?(params[:current_password])
       @user.update_attributes(params[:user])
       @user.update_attribute(:force_change_password, false)
       @user.save!
-      flash[:notice] = "Password has been successfully changed." 
+      flash[:notice] = "Password has been successfully changed."
       redirect_to home_path
     else
-      flash[:error] = "Password authentication failed" 
+      flash[:error] = "Password authentication failed"
       render :action => 'change_password'
     end
-    
+
   rescue ActiveRecord::RecordInvalid
     render :action => 'change_password'
   end
-  
+
   private
-  
+
   def failed_login(message)
     flash[:error] = message
     #      redirect_to(open_id_path(:login))
