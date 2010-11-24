990eb23f0f61f92b7d5e321ade2f96e3ec4baf03
unknown <bsturim@.SCRIBEHQ.local>
2010-11-24 10:22:37 -0500

    Switch to use regular expressions for ip whitelisting for otp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -150,8 +150,8 @@ class UsersController < ApplicationController
   
   def fetch_otp
     user = nil
-    trusted_sites = (APP_CONFIG['otp_trusted_sites'] || "").gsub(/^\s*\(|\)\s*$/, "").split(/,/) || [""]
-    if trusted_sites.include? request.remote_ip 
+    trusted_sites = (APP_CONFIG['otp_trusted_sites'] || "")
+    if Regexp.new(trusted_sites).match(request.remote_ip )
       user = User.find_by_email(params[:login])
     else
       logger.warn("Request for a otp from an untrusted site: #{request.remote_ip} is not in #{APP_CONFIG['otp_trusted_sites']}")
