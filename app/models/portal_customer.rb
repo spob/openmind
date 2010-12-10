9c6f01e0ac2da67900183aa9c595b9fa1c9957dc
Bob Sturim <bob@sturim.org>
2010-12-10 07:07:01 -0500

    Changed datetime to date for nfrs in portal added partner licenses

--- app/models/portal_customer.rb
+++ app/models/portal_customer.rb
@@ -1,4 +1,12 @@
 class PortalCustomer < ActiveRecord::Base  
   belongs_to :portal_org
   default_scope :order => 'serial_number'
+
+  def expires_at
+    maintenance_expires_at
+  end
+
+  def registered_to
+    portal_org.org_name
+  end
 end
