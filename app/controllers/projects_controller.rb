eeeb8c70a2f035abf285833703d00a2c58dd2f0f
Bob Sturim <bob@sturim.org>
2010-12-10 19:56:44 -0500

    Added edit project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,11 @@
 class ProjectsController < ApplicationController
   before_filter :login_required
-  before_filter :fetch_project, :only => [:refresh, :destroy]
+  before_filter :fetch_project, :only => [:refresh, :destroy, :edit, :update]
 
   verify :method      => :post, :only => [:create, :refresh],
          :redirect_to => {:action => :index}
-#  verify :method      => :put, :only => [:update],
-#         :redirect_to => {:action => :index}
+  verify :method      => :put, :only => [:update],
+         :redirect_to => {:action => :index}
   verify :method      => :delete, :only => [:destroy],
          :redirect_to => {:action => :index}
 
@@ -25,6 +25,21 @@ class ProjectsController < ApplicationController
       else
         index
         render :action => 'index'
+      end
+    end
+  end
+
+  def edit
+    can_edit_projects?
+  end
+
+  def update
+    if can_edit_projects?
+      if @project.update_attributes(params[:project])
+        flash[:notice] = "Project #{@project.name} was successfully updated."
+        redirect_to projects_path
+      else
+        render :action => 'edit'
       end
     end
   end
