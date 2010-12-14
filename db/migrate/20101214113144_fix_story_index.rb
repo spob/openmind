d129eec0aca88c0abce62d818c585440b7241265
Bob Sturim <bob@sturim.org>
2010-12-14 06:44:59 -0500

    Fix script

--- db/migrate/20101214113144_fix_story_index.rb
+++ db/migrate/20101214113144_fix_story_index.rb
@@ -4,14 +4,14 @@ class FixStoryIndex < ActiveRecord::Migration
     add_index :stories, [:iteration_id, :pivotal_identifier], :unique => true
 
     remove_index :tasks, [:pivotal_identifier]
-    add_index :tasks, [:iteration_id, :pivotal_identifier], :unique => true
+    add_index :tasks, [:story_id, :pivotal_identifier], :unique => true
   end
 
   def self.down
     remove_index :stories, [:iteration_id, :pivotal_identifier]
     add_index :stories, [:pivotal_identifier], :unique => true
 
-    remove_index :tasks, [:iteration_id, :pivotal_identifier]
+    remove_index :tasks, [:story_id, :pivotal_identifier]
     add_index :tasks, [:pivotal_identifier]
   end
 end
