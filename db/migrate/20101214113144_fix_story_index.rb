f16a1804bb66095f08dd3750ce89f7b0c1ffe49d
Bob Sturim <bob@sturim.org>
2010-12-14 08:01:45 -0500

    Fix script to change indices to drop fk's first

--- db/migrate/20101214113144_fix_story_index.rb
+++ db/migrate/20101214113144_fix_story_index.rb
@@ -1,17 +1,29 @@
-class FixStoryIndex < ActiveRecord::Migration
-  def self.up
-    remove_index :stories, [:pivotal_identifier]
-    add_index :stories, [:iteration_id, :pivotal_identifier], :unique => true
+require "migration_helpers"
 
-    remove_index :tasks, [:pivotal_identifier]
+class FixStoryIndex < ActiveRecord::Migration
+  extend MigrationHelpers
+  
+  def self.up
+    remove_foreign_key(:stories, :iteration_id)
+    add_index :stories, [:iteration_id, :pivotal_identifier], :unique => true
+    remove_index :stories, [:pivotal_identifier]
+    add_foreign_key(:stories, :iteration_id, :iterations)
+
+    remove_foreign_key(:tasks, :story_id)
     add_index :tasks, [:story_id, :pivotal_identifier], :unique => true
+    remove_index :tasks, [:pivotal_identifier]
+    add_foreign_key(:tasks, :story_id, :stories)
   end
 
   def self.down
+    remove_foreign_key(:stories, :iteration_id)
+    add_index :stories, [:pivotal_identifier]
     remove_index :stories, [:iteration_id, :pivotal_identifier]
-    add_index :stories, [:pivotal_identifier], :unique => true
+    add_foreign_key(:stories, :iteration_id, :iterations)
 
-    remove_index :tasks, [:story_id, :pivotal_identifier]
+    remove_foreign_key(:tasks, :story_id)
     add_index :tasks, [:pivotal_identifier]
+    remove_index :tasks, [:story_id, :pivotal_identifier]
+    add_foreign_key(:tasks, :story_id, :stories)
   end
 end
