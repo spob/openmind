57524043706c8f1060ed2e895647415a06990a9b
Bob Sturim <bob@sturim.org>
2010-12-14 06:41:07 -0500

    Fixed index

--- db/migrate/20101214113144_fix_story_index.rb
+++ db/migrate/20101214113144_fix_story_index.rb
@@ -2,10 +2,16 @@ class FixStoryIndex < ActiveRecord::Migration
   def self.up
     remove_index :stories, [:pivotal_identifier]
     add_index :stories, [:iteration_id, :pivotal_identifier], :unique => true
+
+    remove_index :tasks, [:pivotal_identifier]
+    add_index :tasks, [:iteration_id, :pivotal_identifier], :unique => true
   end
 
   def self.down
     remove_index :stories, [:iteration_id, :pivotal_identifier]
     add_index :stories, [:pivotal_identifier], :unique => true
+
+    remove_index :tasks, [:iteration_id, :pivotal_identifier]
+    add_index :tasks, [:pivotal_identifier]
   end
 end
