854e026111958a2b31a234385beb4c1bcce06a05
Bob Sturim <bob@sturim.org>
2010-12-27 16:04:48 -0500

    Don't show NFRs for customers

--- app/models/portal_org.rb
+++ app/models/portal_org.rb
@@ -1,5 +1,5 @@
 class PortalOrg < ActiveRecord::Base
-  has_many :portal_customers, :order => 'maintenance_expires_at DESC'
+  has_many :portal_customers, :conditions => ["portal_customers.serial_number not like ?", 'NFR!%'], :order => 'maintenance_expires_at DESC'
   has_many :portal_certified_consultants, :order => 'consultant_email'
   has_many :portal_nfrs, :order => 'expires_at DESC, serial_number'
   has_many :portal_support_incidents, :order => 'case_number DESC'
