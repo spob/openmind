b9cba1c1a5827b83516fc436bda971cd8f8c7967
Bob Sturim <bob@sturim.org>
2010-12-31 21:43:15 -0500

    Don't show a day 11

--- app/models/iteration.rb
+++ app/models/iteration.rb
@@ -66,6 +66,7 @@ class Iteration < ActiveRecord::Base
   end
 
   def calc_day_number the_date=Project.calculate_project_date
+    the_date = end_on - 1 if the_date > end_on - 1
     day_num = 0
     (self.start_on..the_date).each do |d|
       day_num += 1 if d.cwday < 6
