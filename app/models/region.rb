c77fdca7749a3cf1c53368382b607205b43f4cb0
Bob Sturim <bob@sturim.org>
2011-02-06 22:21:14 -0500

    Added region, etc.

---
+++ app/models/region.rb
@@ -0,0 +1,32 @@
+# == Schema Information
+# Schema version: 20081021172636
+#
+# Table name: lookup_codes
+#
+#  id           :integer(4)      not null, primary key
+#  code_type    :string(30)      not null
+#  short_name   :string(40)      not null
+#  description  :string(50)      not null
+#  sort_value   :integer(4)      default(100), not null
+#  created_at   :datetime        not null
+#  lock_version :integer(4)      default(0)
+#  updated_at   :datetime        not null
+#
+
+class Region < LookupCode
+  has_many :forecasts,
+    :dependent => :destroy
+
+  named_scope :by_short_name, :order => "short_name ASC"
+  named_scope :by_sort_value, :order => "sort_value ASC"
+
+  def can_delete?
+    forecasts.empty?
+  end
+
+  def self.findall include_empty = false
+    list = Region.by_sort_value
+    list.insert(0, Region.new(:id => 0, :short_name => "")) if include_empty
+    list
+  end
+end
