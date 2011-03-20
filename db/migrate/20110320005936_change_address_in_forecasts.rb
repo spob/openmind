05f25e824318bcce58c77d74c6e95fcc9f021000
Bob Sturim <bob@sturim.org>
2011-03-19 21:28:36 -0400

    Coded migration for address changes

---
+++ db/migrate/20110320005936_change_address_in_forecasts.rb
@@ -0,0 +1,47 @@
+class ChangeAddressInForecasts < ActiveRecord::Migration
+  def self.up
+    change_table :forecasts do |t|
+      t.string :address1, :null => true #false
+      t.string :address2, :null => true
+      t.string :city, :null => true #false
+      t.string :state, :null => true
+      t.string :postal_code, :null => true
+      t.string :country, :limit => 2, :null => true #false
+    end
+
+    Forecast.reset_column_information
+
+    Forecast.all.each do |f|
+      f.update_attributes(:address1 => f.location, :city => 'XX', :country => 'XX')
+    end
+
+    change_table :forecasts do |t|
+      t.change :address1, :string, :null => false
+      t.change :city, :string, :null => false
+      t.change :country, :string, :null => false
+      t.remove :location
+    end
+  end
+
+  def self.down
+    change_table :forecasts do |t|
+      t.remove :address1
+      t.remove :address2
+      t.remove :city
+      t.remove :state
+      t.remove :postal_code
+      t.remove :country
+      t.string :location, :null => true
+    end
+
+    Forecast.reset_column_information
+
+    Forecast.all.each do |f|
+      f.update_attributes(:location => f.address1)
+    end
+
+    change_table :forecasts do |t|
+      t.change :location, :string, :null => false
+    end
+  end
+end
